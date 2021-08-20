f238c631afb94be14616ca925d6301d9069e0321
Ishita Goyal <goyalishita0301@gmail.com>
2021-08-20 12:53:08 +0530

    boilerplate code

--- pizzaShopProject/pizzaShopApp/models.py
+++ pizzaShopProject/pizzaShopApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    name=models.CharField(max_length=200,null=True)
+    type=models.CharField(max_length=200,null=True)
+    size=models.IntegerField(default=4,null=True,blank=True)
+    topping=models.CharField(max_length=200,null=True,blank=True)
+    def __str__(self):
+        return str(self.name)
--- pizzaShopProject/pizzaShopProject/settings.py
+++ pizzaShopProject/pizzaShopProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pizzaShopApp',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,18 @@ WSGI_APPLICATION = 'pizzaShopProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'PizzaDB',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

dce072d31a0ba95687db0fcaddba284be7ed1dc0
Ishita Goyal <goyalishita0301@gmail.com>
2021-08-20 14:36:19 +0530

    added rest apis

--- pizzaShopProject/pizzaShopApp/models.py
+++ pizzaShopProject/pizzaShopApp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Pizza(models.Model):
     name=models.CharField(max_length=200,null=True)
     type=models.CharField(max_length=200,null=True)
-    size=models.IntegerField(default=4,null=True,blank=True)
+    size=models.IntegerField(default=10,null=True,blank=True)
     topping=models.CharField(max_length=200,null=True,blank=True)
     def __str__(self):
         return str(self.name)
